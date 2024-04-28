7a6e34a5187bfc42247c9f9fbedd7f65a92520ed
João Pedro Soares <joaopedrosoaresdebrito@gmail.com>
2024-04-28 18:32:28 -0300

    pagina petropolis finalizada

---
+++ js/subpaginas.js
@@ -0,0 +1,16 @@
+function mostra(imageNumber) {
+    var images = document.getElementsByClassName("selected");
+    for (var i = 0; i < images.length; i++) {
+        images[i].style.display = "none";
+    }
+    document.getElementById("image" + imageNumber).style.display = "block";
+
+    var allImages = document.querySelectorAll('.Fotos');
+        for (var i = 0; i < allImages.length; i++) {
+            if (i + 1 !== imageNumber) {
+                allImages[i].classList.add('blur');
+            } else {
+                allImages[i].classList.remove('blur');
+            }
+        }
+}
