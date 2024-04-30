ced9aeb1b35829efac3da7ff3737d21d3db86e3f
João Pedro Soares <joaopedrosoaresdebrito@gmail.com>
2024-04-30 17:33:21 -0300

    feat surprises

--- js/equipe.js
+++ js/equipe.js
@@ -1,12 +1,23 @@
-function mostrarTexto(id) {
-  var texto = document.getElementById(id);
-  
-  // Verificar se o texto está visível
-  if (texto.style.display === 'block') {
-    // Se estiver visível, ocultá-lo
-    texto.style.display = 'none';
-  } else {
-    // Se estiver oculto, exibi-lo
-    texto.style.display = 'block';
+  function mostrarTexto(id) {
+    var texto = document.getElementById(id);
+    
+    // Verificar se o texto está visível
+    if (texto.style.display === 'block') {
+      // Se estiver visível, ocultá-lo
+      texto.style.display = 'none';
+    }
+    
+    else {
+      // Se estiver oculto, exibi-lo
+      texto.style.display = 'block';
+     
+    }
   }
-}
+  function toca(id){
+    if (id=='queridao'){
+      var audio = document.getElementById('clickSound');
+      audio.play();
+    }
+  }
+
+
