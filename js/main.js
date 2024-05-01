a8467b5e3a24ccd8f5698b5cdeea359a2d12a219
João Pedro Soares <joaopedrosoaresdebrito@gmail.com>
2024-05-01 13:09:11 -0300

    feat: responsivity in index

---
+++ js/main.js
@@ -0,0 +1,4 @@
+document.getElementById('enviarModal').addEventListener('click', function() {
+    //  Adicionar codigo que envie para um json
+        document.getElementById('formModal').reset();
+      });
