c8d8921f3a054ade7b9a0103eaa3083e0cec55a0
João Pedro Soares <joaopedrosoaresdebrito@gmail.com>
2024-05-03 17:51:25 -0300

    PUSH: final version ZEPEX

--- js/nightmode.js
+++ js/nightmode.js
@@ -5,4 +5,16 @@ nightModeButton.addEventListener('click', function() {
   body.classList.toggle('night-mode'); 
 });
 
+nightModeButton.addEventListener('change', function() {
 
+  localStorage.setItem('nightMode', body.classList.contains('night-mode'));
+});
+
+window.onload = function() {
+  const isNightMode = localStorage.getItem('nightMode') === 'true';
+
+
+  if (isNightMode) {
+    body.classList.add('night-mode');
+  }
+};
