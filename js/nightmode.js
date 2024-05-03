eaf493c7e0b85ea8e733cb942207933dcfbc9969
João Pedro Soares <joaopedrosoaresdebrito@gmail.com>
2024-05-03 14:00:48 -0300

    feat: modais de venda

--- js/nightmode.js
+++ js/nightmode.js
@@ -1,8 +1,8 @@
-const nightModeButton = document.getElementById('nightModeCheckbox'); // Update ID
+const nightModeButton = document.getElementById('nightModeCheckbox'); 
 const body = document.body;
 
 nightModeButton.addEventListener('click', function() {
-  body.classList.toggle('night-mode'); // Toggle the night-mode class on/off
+  body.classList.toggle('night-mode'); 
 });
 
 
